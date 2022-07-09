1f197f22a645d79f8f2bd961ab451687011172e0
José Roberto de Badran Borges <betobadran@live.com>
2022-07-09 16:28:02 +0000

    add test

---
+++ src/main/App.cs
@@ -0,0 +1,11 @@
+﻿namespace br.com.badr.etd
+{
+    public class App
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("TESTE write");
+        }
+    }
+}
---
+++ src/test/AppTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace br.com.badr.etd
+{
+    public class AppTest {
+        [SetUp]
+        public void setup() {
+            Console.WriteLine("setup");
+        }
+
+        [Test]
+        public void test() {
+            Assert.Pass();
+        }
+    }
+}
